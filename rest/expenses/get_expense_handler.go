a84337cd7051fef9ad7a8e6c4c7f8f88a301b277
PatcharaKL <patchara.pck@gmail.com>
2023-01-02 00:06:23 +0700

    [Edit] Add get all expenses handler

--- rest/expenses/get_expense_handler.go
+++ rest/expenses/get_expense_handler.go
@@ -7,7 +7,7 @@ import (
 	"github.com/lib/pq"
 )
 
-func (h *handler) GetExpensesByIdHandler(c echo.Context) error {
+func (h *handler) GetExpenseByIdHandler(c echo.Context) error {
 	id := c.Param("id")
 
 	row := h.DB.QueryRow("SELECT * FROM expenses WHERE id = $1", id)
@@ -19,4 +19,23 @@ func (h *handler) GetExpensesByIdHandler(c echo.Context) error {
 	}
 
 	return c.JSON(http.StatusOK, e)
+}
+
+func (h *handler) GetExpensesHandler(c echo.Context) error {
+	stmt, err := h.DB.Prepare("SELECT * FROM expenses")
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "can't prepare query all users statement:" + err.Error()})
+	}
+	rows, err := stmt.Query()
+	expenses := []Expenses{}
+	for rows.Next() {
+		e := Expenses{}
+		err = rows.Scan(&e.ID, &e.Title, &e.Amount, &e.Note, pq.Array(&e.Tags))
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, Err{Message: "can't scan user:" + err.Error()})
+		}
+		expenses = append(expenses, e)
+	}
+
+	return c.JSON(http.StatusOK, expenses)
 }
