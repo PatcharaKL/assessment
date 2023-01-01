28e9672015452166dedb08e4a21c720d4855eeb9
PatcharaKL <patchara.pck@gmail.com>
2023-01-01 18:53:09 +0700

    [Edit] delete prepare query and use inline instead, also fix pq.Array to take pointer intead

--- rest/expenses/get_expense_handler.go
+++ rest/expenses/get_expense_handler.go
@@ -10,18 +10,13 @@ import (
 func (h *handler) GetExpensesByIdHandler(c echo.Context) error {
 	id := c.Param("id")
 
-	stmt, err := h.DB.Prepare("SELECT * FROM expenses WHERE id = $1")
-	if err != nil {
-		return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
-	}
-
-	row := stmt.QueryRow(id)
+	row := h.DB.QueryRow("SELECT * FROM expenses WHERE id = $1", id)
 
 	e := Expenses{}
-	err = row.Scan(&e.ID, &e.Title, &e.Amount, &e.Note, pq.Array(e.Tags))
+	err := row.Scan(&e.ID, &e.Title, &e.Amount, &e.Note, pq.Array(&e.Tags))
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, Err{Message: err.Error()})
 	}
 
-	return c.JSON(http.StatusCreated, e)
+	return c.JSON(http.StatusOK, e)
 }
