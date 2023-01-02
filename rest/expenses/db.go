44c046ad6fa711c1ef59e2b903fb93b782c915a7
PatcharaKL <patchara.pck@gmail.com>
2023-01-02 14:09:22 +0700

    [Refactoring] extract sql script

--- rest/expenses/db.go
+++ rest/expenses/db.go
@@ -8,13 +8,8 @@ import (
 	_ "github.com/lib/pq"
 )
 
-func InitDB() *sql.DB {
-	var err error
-	db, err := sql.Open("postgres", os.Getenv("DATABASE_STR"))
-	if err != nil {
-		log.Fatal("Connect to database error", err)
-	}
-	createTb := `
+const (
+	createExpensesTableSQL = `
 	CREATE TABLE IF NOT EXISTS expenses (
 		id SERIAL PRIMARY KEY,
 		title TEXT,
@@ -23,7 +18,19 @@ func InitDB() *sql.DB {
 		tags TEXT[]
 	);
 	`
-	_, err = db.Exec(createTb)
+	createExpenseSQL = "INSERT INTO expenses (title, amount, note, tags) values ($1, $2, $3, $4) RETURNING id;"
+	getExpensesSQL   = "SELECT * FROM expenses"
+	getExpenseSQL    = "SELECT * FROM expenses WHERE id = $1"
+	updateExpenseSQL = "UPDATE expenses SET title = $2, amount = $3, note = $4, tags = $5 WHERE id = $1"
+)
+
+func InitDB() *sql.DB {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_STR"))
+	if err != nil {
+		log.Fatal("Connect to database error", err)
+	}
+
+	_, err = db.Exec(createExpensesTableSQL)
 
 	if err != nil {
 		log.Fatal("can't create table", err)
