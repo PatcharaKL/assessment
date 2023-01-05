1a1f8a262d59f778347f1867e0e39f9351aab23d
PatcharaKL <patchara.pck@gmail.com>
2023-01-05 16:09:40 +0700

    [Hotfix] fix variable type

--- rest/expenses/expenses.go
+++ rest/expenses/expenses.go
@@ -5,7 +5,7 @@ import "database/sql"
 type Expenses struct {
 	ID     int      `json:"id"`
 	Title  string   `json:"title"`
-	Amount int      `json:"amount"`
+	Amount float64  `json:"amount"`
 	Note   string   `json:"note"`
 	Tags   []string `json:"tags"`
 }
